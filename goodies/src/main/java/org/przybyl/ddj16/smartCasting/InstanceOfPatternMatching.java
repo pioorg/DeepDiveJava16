d1a1e16692296f75af0a6d466f91e7e4a7ede278
Piotr <piotr@przybyl.org>
2021-04-06 16:14:26 +0200

    Tuned examples for Pattern Matching with instanceof.

--- goodies/src/main/java/org/przybyl/ddj16/smartCasting/InstanceOfPatternMatching.java
+++ goodies/src/main/java/org/przybyl/ddj16/smartCasting/InstanceOfPatternMatching.java
@@ -22,35 +22,72 @@ import java.time.*;
 
 public class InstanceOfPatternMatching {
 
-	public static void main(String[] args) {
-		whoAreYou(Instant.now());
-		whoAreYou("Hi");
-		whoAreYou("Hello");
-		whoAreYou(-2);
-		whoAreYou(12);
-		whoAreYou(777L);
+    private static Long aLong = 0L;
 
-		System.out.printf("Is 12 a non empty String? %b%n", nonEmptyString(12));
-		System.out.printf("""
-         Is "12" a non empty String? %b%n""", nonEmptyString("12"));
-		System.out.printf("""
-         Is "" a non empty String? %b%n""", nonEmptyString(""));
-	}
+    public static void main(String[] args) {
+        whoAreYou(Instant.now());
+        whoAreYou("Hi");
+        whoAreYou("Hello");
+        whoAreYou(-2);
+        whoAreYou(12);
+        whoAreYou(777L);
 
-	public static void whoAreYou(Object obj) {
-		if (obj instanceof Serializable serializable) {
-			System.out.println("You're serializable " + serializable + "!");
-		}
-		if (obj instanceof String string && string.length() >= 3) {
-			System.out.println("You're String, at least 3 characters long!");
-		} else if (obj instanceof Integer integer && integer > 0) {
-			System.out.println("You're a positive Integer");
-		} else if (obj instanceof Long aLong && (aLong < -9 || aLong > 9)) {
-			System.out.println("You're a Long with at least two digits");
-		}
-	}
+        System.out.printf("Is 12 a non empty String? %b%n", nonEmptyString(12));
+        System.out.printf("""
+            Is "12" a non empty String? %b%n""", nonEmptyString("12"));
+        System.out.printf("""
+            Is "" a non empty String? %b%n""", nonEmptyString(""));
 
-	public static boolean nonEmptyString(Object obj) {
-		return (obj instanceof String str) && !str.isEmpty();
-	}
+        for (int i = 0; i < 10; i++) {
+            notFinal();
+        }
+    }
+
+    public static void whoAreYou(Object obj) {
+        if (obj instanceof Serializable serializable) {
+            System.out.println("You're serializable " + serializable + "!");
+        }
+        if (obj instanceof String string && string.length() >= 3) {
+            System.out.println("You're String, at least 3 characters long!");
+        } else if (obj instanceof Integer integer && integer > 0) {
+            System.out.println("You're a positive Integer");
+        } else if (obj instanceof Long aLong && (aLong < -9 || aLong > 9)) {
+            System.out.println("You're a Long with at least two digits");
+        }
+    }
+
+    public static boolean nonEmptyString(Object obj) {
+        return (obj instanceof String str) && !str.isEmpty();
+    }
+
+    public static void notFinal() {
+        var number = getRandomNumber();
+        if (number instanceof Long aLong) {
+            aLong = 42L;
+            System.out.println(aLong);
+        } else if (number instanceof Float aFloat) {
+            aFloat = 42f;
+            System.out.println(aFloat);
+        }
+//        System.out.println(aLong);
+    }
+
+    private static Object getRandomNumber() {
+        if (Math.random() < 0.5) {
+            return 17L;
+        } else {
+            return 15f;
+        }
+    }
+
+    public static void pointlessChecks() {
+//		Long aLong = 42L;
+//		if (aLong instanceof Number number) {
+//			System.out.println("This should not work.");
+//		}
+//
+//		if (aLong instanceof String str) {
+//			System.out.println("This should not work.");
+//		}
+    }
 }
