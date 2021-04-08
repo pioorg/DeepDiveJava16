925051d071d854cc7776a85436ba4088b9ea2eda
Piotr <piotr@przybyl.org>
2021-04-08 16:47:49 +0200

    Tuned example for ForeignMemAPI

--- goodies/src/main/java/org/przybyl/ddj16/foreignMemAccess/ForeignMemoryAccessAPIDemo.java
+++ goodies/src/main/java/org/przybyl/ddj16/foreignMemAccess/ForeignMemoryAccessAPIDemo.java
@@ -1,33 +1,75 @@
 package org.przybyl.ddj16.foreignMemAccess;
 
 
-import java.lang.invoke.*;
-import java.nio.*;
+import java.lang.invoke.VarHandle;
+import java.nio.ByteOrder;
 
-import jdk.incubator.foreign.*;
+import jdk.incubator.foreign.MemoryAccess;
+import jdk.incubator.foreign.MemoryAddress;
+import jdk.incubator.foreign.MemoryHandles;
+import jdk.incubator.foreign.MemoryLayout;
+import jdk.incubator.foreign.MemorySegment;
+import jdk.incubator.foreign.SequenceLayout;
 
 /**
- * Demo of Foreign Memory Access API incubator feature of Java 15, second preview,
- * based on https://openjdk.java.net/jeps/383
- *
+ * Demo of Foreign Memory Access API incubator feature of Java 16, 3rd preview,
+ * based on https://openjdk.java.net/jeps/392
+ * <p>
  * Please don't forget to --add-modules jdk.incubator.foreign
  */
 public class ForeignMemoryAccessAPIDemo {
 
-	public static void main(String[] args) {
-		SequenceLayout intArrayLayout = MemoryLayout.ofSequence(100, MemoryLayout.ofValueBits(32, ByteOrder.nativeOrder()));
-		VarHandle intElemHandle = intArrayLayout.varHandle(int.class, MemoryLayout.PathElement.sequenceElement());
+    private static MemorySegment readerSegment;
 
-		try (MemorySegment segment = MemorySegment.allocateNative(intArrayLayout)) {
-			MemoryAddress base = segment.address();
-			for (int i = 0; i < intArrayLayout.elementCount().getAsLong(); i++) {
-				intElemHandle.set(base, (long) i, i);
-			}
+    public static void main(String[] args) throws InterruptedException {
+        sameThreadDemo();
+        twoThreadsDemo();
+    }
 
-			for (int i = 0; i < intArrayLayout.elementCount().getAsLong(); i++) {
-				System.out.println(intElemHandle.get(base, (long) i));
-			}
+    private static void sameThreadDemo() {
+        SequenceLayout intArrayLayout = MemoryLayout.ofSequence(10, MemoryLayout.ofValueBits(32, ByteOrder.nativeOrder()));
+        try (MemorySegment segment = MemorySegment.allocateNative(intArrayLayout)) {
+            populateNativeMem(intArrayLayout, segment);
+            examineNativeMem(intArrayLayout, segment);
+        }
+    }
 
-		}
-	}
+    private static void twoThreadsDemo() throws InterruptedException {
+        SequenceLayout intArrayLayout = MemoryLayout.ofSequence(10, MemoryLayout.ofValueBits(32, ByteOrder.nativeOrder()));
+        MemorySegment segment = MemorySegment.allocateNative(intArrayLayout);
+        populateNativeMem(intArrayLayout, segment);
+        examineNativeMem(intArrayLayout, segment);
+        var readerThread = new Thread(() -> examineAndClose(intArrayLayout, readerSegment), "readerThread");
+        readerSegment = segment.handoff(readerThread);
+        readerThread.start();
+//        examineNativeMem(intArrayLayout, segment);
+    }
+
+    private static void populateNativeMem(SequenceLayout intArrayLayout, MemorySegment segment) {
+        VarHandle intHandle = MemoryHandles.varHandle(int.class, ByteOrder.nativeOrder());
+        VarHandle intElemHandle = intArrayLayout.varHandle(int.class, MemoryLayout.PathElement.sequenceElement());
+        for (int i = 0; i < intArrayLayout.elementCount().getAsLong(); i++) {
+            MemoryAccess.setIntAtOffset(segment, i * 4L, i);
+//                intHandle.set(segment, i * 4L, i);
+//                intElemHandle.set(segment, (long) i, i);
+        }
+    }
+
+    private static void examineNativeMem(SequenceLayout intArrayLayout, MemorySegment segment) {
+        MemoryAddress segmentBaseAddress = segment.address();
+        System.out.println("Examining memory at: " + segmentBaseAddress.toRawLongValue());
+        System.out.println("Segment owned by: " + segment.ownerThread().getName());
+        VarHandle intElemHandle = intArrayLayout.varHandle(int.class, MemoryLayout.PathElement.sequenceElement());
+        for (int i = 0; i < intArrayLayout.elementCount().getAsLong(); i++) {
+            System.out.print(MemoryAccess.getIntAtOffset(segment, i * 4L));
+            System.out.print(" ");
+            System.out.println(intElemHandle.get(segment, (long) i));
+        }
+    }
+
+    private static void examineAndClose(SequenceLayout intArrayLayout, MemorySegment readerSegment) {
+        examineNativeMem(intArrayLayout, readerSegment);
+        readerSegment.close();
+//        examineNativeMem(intArrayLayout, readerSegment);
+    }
 }
